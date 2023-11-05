7d4dc5741d114d325921dd4aae12b53b30674d67
Wallace Júnior <wflj1997@gmail.com>
2023-11-05 15:06:47 -0300

    Contexto do carrinho

--- src/app/(store)/layout.tsx
+++ src/app/(store)/layout.tsx
@@ -1,4 +1,5 @@
 import { Header } from "~/components/Header";
+import { CartContextProvider } from "~/contexts/CartContext";
 
 interface StoreLayoutProps {
   children: React.ReactNode;
@@ -7,8 +8,10 @@ interface StoreLayoutProps {
 function StoreLayout({ children }: StoreLayoutProps): JSX.Element | null {
   return (
     <div className="grid min-h-screen w-full max-w-[1600px] mx-auto grid-rows-[min-content_max-content] gap-5 p-8">
-      <Header />
-      {children}
+      <CartContextProvider>
+        <Header />
+        {children}
+      </CartContextProvider>
     </div>
   );
 }
---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { createContext, useCallback, useContext, useState } from "react";
+
+interface CartItem {
+  productId: number;
+  quantity: number;
+}
+
+interface CartContextData {
+  items: CartItem[];
+  addToCart: (productId: CartItem["productId"]) => void;
+}
+
+const CartContext = createContext({} as CartContextData);
+
+export const useCartContext = () => useContext(CartContext);
+
+interface CartContextProviderProps {
+  children: React.ReactNode;
+}
+
+export const CartContextProvider: React.FC<CartContextProviderProps> = ({
+  children,
+}) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const addToCart: CartContextData["addToCart"] = useCallback(productId => {
+    setCartItems(currentCartItems => {
+      const existsInCart = currentCartItems.some(
+        item => item.productId === productId,
+      );
+
+      if (existsInCart) {
+        return currentCartItems.map(item => {
+          if (item.productId === productId) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          }
+
+          return item;
+        });
+      }
+
+      return currentCartItems.concat({ productId, quantity: 1 });
+    });
+  }, []);
+
+  return (
+    <CartContext.Provider
+      value={{
+        addToCart,
+        items: cartItems,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
